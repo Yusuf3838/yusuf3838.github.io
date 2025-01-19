2c6e5cafafcf6a291837ed1550b7c94673296a94
Yusuf Sheikhali <sheikhay@sheridancollege.ca>
2025-01-19 00:53:44 -0500

    update

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/yusuf3838.github.io/',
-})
+  base: '/my-portfolio/', // Update this to your repository name
+});
